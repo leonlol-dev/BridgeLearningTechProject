7f394701d4bed96ec250e39674e0c3fbe64e5f66
Leon <59918677+leonlol-dev@users.noreply.github.com>
2023-01-08 12:12:25 +0000

    working wall script

---
+++ Assets/WallScript.cs
@@ -0,0 +1,50 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WallScript : MonoBehaviour
+{
+    [SerializeField] private GameObject[] walls;
+
+    //Public 
+    public float spawnRate = 0.5f;
+    //Private
+    private int index = 0;
+    private float nextTimeToActivate = 5f;
+
+    private void Start()
+    {
+        Shuffle(walls);
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        if(Time.time >= nextTimeToActivate)
+        {
+            nextTimeToActivate = Time.time + 1f / (spawnRate);
+            walls[index].SetActive(true);
+            if (index < walls.Length-1)
+            {
+                index++;
+            }
+        }
+    }
+
+    private void Shuffle(GameObject[] a)
+    {
+        // Loops through array
+        for (int i = a.Length - 1; i > 0; i--)
+        {
+            // Randomize a number between 0 and i (so that the range decreases each time)
+            int rnd = Random.Range(0, i);
+
+            // Save the value of the current i, otherwise it'll overright when we swap the values
+            GameObject temp = a[i];
+
+            // Swap the new and old values
+            a[i] = a[rnd];
+            a[rnd] = temp;
+        }
+    }
+}

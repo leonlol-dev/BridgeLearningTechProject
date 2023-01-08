88dd0980222dea4b6c55841b6df4fbd791ff2fb3
Leon <59918677+leonlol-dev@users.noreply.github.com>
2023-01-08 13:09:55 +0000

    game over mechanic wip

---
+++ Assets/PlayerWallDetection.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerWallDetection : MonoBehaviour
+{
+    [SerializeField] private PlayerCubeTrigger left;
+    [SerializeField] private PlayerCubeTrigger right;
+    [SerializeField] private PlayerCubeTrigger top;
+    [SerializeField] private PlayerCubeTrigger bottom;
+
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        if(left.activated == true && right.activated == true && top.activated == true && bottom.activated == true)
+        {
+            Debug.Log("game over");
+        }
+    }
+}

925f6a79cde8099f02a68b90922f4167be9675bf
Leon <59918677+leonlol-dev@users.noreply.github.com>
2023-01-08 22:34:34 +0000

    Update GameManager.cs

--- Assets/GameManager.cs
+++ Assets/GameManager.cs
@@ -4,7 +4,46 @@ using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
+    public enum type
+    {
+        Barrel,
+        Capsule,
+        Sphere,
+        Cube
+    }
+    public type scoreObject;
+    public GameObject doubleScoreObject { get; set; }
     public int score { get; set; }
 
+    public void DoubleScoreObject()
+    {
+        int chance = Random.Range(1, 4);
+        switch (chance)
+        {
+            case 1:
+
+                break;
+
+            case 2:
+
+                break;
+
+            case 3:
+              
+                break;
+
+            case 4:
+
+                break;
+        }
+    }
+
+    public void CheckDoubleScore(GameObject spawnObject)
+    {
+        if(spawnObject.gameObject.name == doubleScoreObject.gameObject.name)
+        {
+            score += 1;
+        }
+    }
 
 }
